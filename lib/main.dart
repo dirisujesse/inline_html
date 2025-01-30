3e5af75157976d126227f8418f3d13c749a4f1f8
Jesse Dirisu <dirisujesse@gmail.com>
2025-01-30 15:14:43 +0100

    modifies base href for github page
    Signed-off-by: Jesse Dirisu <dirisujesse@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:inline_html/inline_html.dart';
 
 //TODO: 3. Final Result:
 //  - Submit your code as a link to a GitHub repository.
-//  - Deploy the compiled web version of the application on GitHub Pages.
 //  - Ensure the code is formatted and documented according to the guidelines in 'Effective Dart' (https://dart.dev/effective-dart/documentation).
 
 void main() {
