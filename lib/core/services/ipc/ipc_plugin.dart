1ef231dc751fc2d83d41b8d6b463a04ae93ee3f9
Jesse Dirisu <dirisujesse@gmail.com>
2025-01-30 16:00:49 +0100

    Addition of documentation comments
    Signed-off-by: Jesse Dirisu <dirisujesse@gmail.com>

--- lib/core/services/ipc/ipc_plugin.dart
+++ lib/core/services/ipc/ipc_plugin.dart
@@ -12,13 +12,21 @@ abstract class IpcPlugin {
     return _instance!;
   }
 
+  /// The label for the generated `<img>` element
   String get imageElement;
+  /// The id assigned the generated `<img>` element
   String get imageElementId;
 
+  /// Modifies the display of of the `<img>` element 
+  /// [url] is a nullable string matching an image resource
+  /// [url] is set as the `src` of the image element
   void toggleImageElement(String? url);
 
+  /// Bootstraps the `<img>` element
   void createImageElement();
 
+  /// Toggles the fullscreen display of the web document
+  /// [fullscreen] specifies if the document window should be in fullscreen or not
   void modifyWindowDisplay({
     bool? fullscreen,
     OnChanged<bool>? onModified,
