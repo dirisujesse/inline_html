97955b65ce0378239943cd5ad8e4df9452d7a0fa
Jesse Dirisu <dirisujesse@gmail.com>
2025-01-30 14:06:47 +0100

    - removes completed todo item
    Signed-off-by: Jesse Dirisu <dirisujesse@gmail.com>

--- lib/app/modules/home/presentation/ui/mixins/home_mixin.dart
+++ lib/app/modules/home/presentation/ui/mixins/home_mixin.dart
@@ -2,10 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:inline_html/core/services/ipc/ipc_plugin.dart';
 import 'package:inline_html/inline_html.dart';
 
-//TODO: 1. URL Input Field and Image Display:
-//  Create an input field for a URL and a button. When the button is clicked, an image should appear in the center of the application using an HTML `< img >` element (not a Flutter `Image` widget).
-//  - Double-clicking on the image should toggle the browser's fullscreen mode. Use JS functions called directly from Dart for this functionality.
-
 mixin HomeMixin<T extends HomeScreen> on State<T> {
   late final ValueNotifier<bool> formStateEmitter;
   late final ValueNotifier<bool> inFullScreen;
