88db6f5bf337b52c4897263806adec4e1a44f8a9
Jesse Dirisu <dirisujesse@gmail.com>
2025-01-30 15:09:27 +0100

    Creates doc folder for github pages
    Signed-off-by: Jesse Dirisu <dirisujesse@gmail.com>

---
+++ docs/scripts/window_toggle.js
@@ -0,0 +1,4 @@
+async function toggleFullscreen(callback) {
+  const isInFullScreen = document.fu
+  setTimeout(async (_) => callback(JSON.stringify(await data.json())), 5000);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:inline_html/inline_html.dart';
 
-//TODO: 2. "Plus" Button and Context Menu:
-//  Add a button with a "Plus" icon to the bottom-right corner of the screen.
-//  - Clicking on this button should display a context menu with "Enter fullscreen" and "Exit fullscreen" buttons above it.
-//  - When the menu is open, the background should dim while the button and menu remain highlighted.
-//  - Clicking outside the menu should close it, and clicking the menu buttons should perform the corresponding action and close the menu.
-
 //TODO: 3. Final Result:
 //  - Submit your code as a link to a GitHub repository.
 //  - Deploy the compiled web version of the application on GitHub Pages.
